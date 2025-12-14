d9c53be233181e06960c16dcfcf203977c6bbcd6
akshatanepal17 <nepal030akshata71@gmail.com>
2025-12-14 11:40:00 +0545

    fate: Footer

--- resources/js/Components/custom-components/footer.jsx
+++ resources/js/Components/custom-components/footer.jsx
@@ -1,10 +1,12 @@
+import { Mail, Phone } from "lucide-react";
+
 function Footer() {
     return (
-        <footer className="bg-white py-16">
-            <div className="container mx-auto px-6 grid grid-cols-1 md:grid-cols-4 gap-10">
+        <footer className="bg-white pt-10">
+            <div className=" grid grid-cols-1 md:grid-cols-2 gap-10">
 
-                <div>
-                    <div className="flex items-center space-x-2 mb-4">
+                <div className="p-20">
+                    <div className="flex items-center space-x-2 mb-4 ">
                         <img src="/images/best-nepal-logo.png" alt="Best Nepal Logo" className="h-10" />
                         <span className="text-xl font-semibold text-gray-800">Best Nepal</span>
                     </div>
@@ -18,28 +20,34 @@ function Footer() {
                     </button>
                 </div>
 
-                <div>
-                    <h3 className="font-semibold text-gray-800 mb-4">Menu</h3>
-                    <ul className="space-y-2 text-gray-600">
-                        <li><a href="/careers" className="text-blue-600">Careers</a></li>
-                        <li><a href="/about" className="text-blue-600">About Us</a></li>
-                        <li><a href="/blog" className="text-blue-600">Blog</a></li>
-                        <li><a href="/contact" className="text-blue-600">Contact Us</a></li>
-                        <li><a href="/privacy" className="text-blue-600">Privacy Policy</a></li>
-                    </ul>
-                </div>
+                <div className="flex gap-x-20 bg-[#E2F4FF] p-6 rounded-tl-3xl">
+                    <div>
+                        <h3 className="font-semibold text-gray-800 mb-4">Menu</h3>
+                        <ul className="space-y-2 text-gray-600">
+                            <li><a href="/careers" className="text-blue-600">Careers</a></li>
+                            <li><a href="/about" className="text-blue-600">About Us</a></li>
+                            <li><a href="/blog" className="text-blue-600">Blog</a></li>
+                            <li><a href="/contact" className="text-blue-600">Contact Us</a></li>
+                            <li><a href="/privacy" className="text-blue-600">Privacy Policy</a></li>
+                        </ul>
+                    </div>
 
-                <div>
-                    <h3 className="font-semibold text-gray-800 mb-4">Reach Us</h3>
-                    <p className="text-gray-600">
-                        K-10 Tower, 10th Floor,<br /> Lazimpat, Kathmandu
-                    </p>
-                </div>
 
-                <div>
-                    <h3 className="font-semibold text-gray-800 mb-4">Contact</h3>
-                    <p className="text-gray-600 mb-2">bestnepalcompany@email.com</p>
-                    <p className="text-gray-600">+977 9851032081</p>
+                    <div>
+                        <h3 className="font-semibold text-gray-800 mb-4">Reach Us</h3>
+                        <div className="space-y-6">
+                            
+                        <p className="text-gray-600">
+                            K-10 Tower, 10th Floor,<br /> Lazimpat, Kathmandu
+                        </p>
+                        <p className=" flex gap-x-1 text-gray-600 mb-2">
+                            <Mail className="w-5" /> bestnepalcompany@email.com</p>
+                        <p className="flex gap-x-1 text-gray-600">
+                            <Phone className="w-5"/>
+                            +977 9851032081</p>
+                        </div>
+                    </div>
+
                 </div>
             </div>
         </footer>
